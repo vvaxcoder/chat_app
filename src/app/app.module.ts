f1f8ab12af177c601ddaa214b90662fd4d04e681
valeriivoronkov <scorpirat@gmail.com>
2019-12-15 20:51:38 +0300

    Add folders components/authTabs with auth-tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthTabsComponent } from './components/auth-tabs/auth-tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthTabsComponent
   ],
   imports: [
     BrowserModule,
