a58135bbb2085d776aaaa40559c809a9966a35fb
valeriivoronkov <scorpirat@gmail.com>
2019-12-15 21:40:06 +0300

    Add AuthTabsComponent to declarations section

--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthTabsComponent],
   exports: [
     AuthTabsComponent
   ],
