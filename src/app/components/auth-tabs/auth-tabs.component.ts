6397c702a2251c189fcc3723c974f729eaa7851d
valeriivoronkov <scorpirat@gmail.com>
2019-12-15 22:54:09 +0300

    initialize and setup materialize tabs

--- src/app/components/auth-tabs/auth-tabs.component.ts
+++ src/app/components/auth-tabs/auth-tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-auth-tabs',
@@ -10,6 +11,9 @@ export class AuthTabsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const tabs = document.querySelector('.tabs');
+
+    M.Tabs.init(tabs, {});
   }
 
 }
