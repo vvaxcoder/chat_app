d32db5936141301f7751c7222cb2bba7fd177b00
valeriivoronkov <scorpirat@gmail.com>
2019-12-15 21:33:40 +0300

    Start to setting up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { AuthRoutingModule } from './modules/auth-routing.module';
+import { AuthModule } from './modules/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthTabsComponent } from './components/auth-tabs/auth-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthTabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AuthModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/auth.module.ts
@@ -0,0 +1,14 @@
+import { AuthTabsComponent } from './../components/auth-tabs/auth-tabs.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    AuthTabsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
