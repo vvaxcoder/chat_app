5981ccf0368e2a2223b855b8d1545ee0208b104d
valeriivoronkov <scorpirat@gmail.com>
2019-12-24 12:19:58 +0300

    Create auth route and add it in server.js

---
+++ chatapp_backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const AuthController = require('../controllers/auth');
+
+router.post('signup', AuthController.createUser);
+
+module.exports = router;
--- chatapp_backend/server.js
+++ chatapp_backend/server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 const config = require("./config");
+
+const auth = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cookieParser());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+
+app.use("/api/chatapp", auth);
 
 mongoose.Promise = global.Promise;
 
@@ -17,4 +21,6 @@ mongoose.connect(config.bd_url, {
   useNewUrlParser: true
 });
 
-app.listen(config.port, () => { console.log(`Running on port ${config.port}`); });
+app.listen(config.port, () => {
+  console.log(`Running on port ${config.port}`);
+});
