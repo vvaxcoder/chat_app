3dfc4b5e61a7f0b7e133b5a08ecad4cb12b9d905
valeriivoronkov <scorpirat@gmail.com>
2019-12-27 12:46:24 +0300

    Add http-status-codes package

--- chatapp_backend/controllers/auth.js
+++ chatapp_backend/controllers/auth.js
@@ -1,24 +1,24 @@
 const Joi = require("@hapi/joi");
+const HttpStatus = require('http-status-codes')
 
 module.exports = {
   createUser(req, resp) {
-    const schema = Joi.object()
-      .keys({
-        username: Joi.string()
-          .alphanum()
-          .min(5)
-          .max(15)
-          .required(),
-        email: Joi.string().email({ minDomainAtoms: 2 }),
-        password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/)
-      })
-      .with("username", "birthyear")
-      .without("password", "access_token");
+    const schema = Joi.object({
+      username: Joi.string()
+        .alphanum()
+        .min(5)
+        .max(15)
+        .required(),
+      email: Joi.string().email(),
+      password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/)
+    });
 
-      const { error, value } = Joi.ValidationError(req.body, schema);
+      const { username, email, password } = req.body;
+
+      const { error, value } = schema.validate({ username, email, password });
 
       if (error && error.details) {
-          return resp.status(500).json({ message: error.details });
+          return resp.status(HttpStatus.BAD_REQUEST).json({ message: error.details });
       }
   }
 };
