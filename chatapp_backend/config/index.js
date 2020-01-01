d2c2aaa0683543c241bc9475322dc4437f81d8ed
valeriivoronkov <scorpirat@gmail.com>
2020-01-01 12:52:40 +0300

    Add jsonwebtoken package and add setup to create token when user has been registered

--- chatapp_backend/config/index.js
+++ chatapp_backend/config/index.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 module.exports = {
     port: process.env.PORT || 3028,
-    db_url: process.env.DB_URL
+    db_url: process.env.DB_URL,
+    secret: process.env.SECRET
 }
--- chatapp_backend/controllers/auth.js
+++ chatapp_backend/controllers/auth.js
@@ -1,8 +1,10 @@
 const Joi = require("@hapi/joi");
-const HttpStatus = require('http-status-codes');
+const HttpStatus = require("http-status-codes");
 const User = require("../models/userModel");
 const transformCases = require("../helpers/transformCases");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { secret } = require('../config');
 
 module.exports = {
   async createUser(req, resp) {
@@ -16,29 +18,41 @@ module.exports = {
       password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/)
     });
 
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
-    const {error, value} = schema.validate({username, email, password});
+    const { error, value } = schema.validate({ username, email, password });
 
     if (error && error.details) {
-      return resp.status(HttpStatus.BAD_REQUEST).json({message: error.details});
+      return resp
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ message: error.details });
     }
 
-    const userEmail = await User.findOne({email: transformCases.firstLetterLowercase(value.email)});
+    const userEmail = await User.findOne({
+      email: transformCases.firstLetterLowercase(value.email)
+    });
 
     if (userEmail) {
-      return resp.status(HttpStatus.CONFLICT).json({message: 'Email already exists!'});
+      return resp
+        .status(HttpStatus.CONFLICT)
+        .json({ message: "Email already exists!" });
     }
 
-    const userName = await User.findOne({username: transformCases.firstLetterUppercase(value.username)});
+    const userName = await User.findOne({
+      username: transformCases.firstLetterUppercase(value.username)
+    });
 
     if (userName) {
-      return resp.status(HttpStatus.CONFLICT).json({message: 'userName already exists!'});
+      return resp
+        .status(HttpStatus.CONFLICT)
+        .json({ message: "userName already exists!" });
     }
 
     return bcrypt.hash(value.password, 12, (err, hash) => {
       if (error) {
-        return resp.status(HttpStatus.BAD_REQUEST).json({message: 'Error hashing password'});
+        return resp
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: "Error hashing password" });
       }
 
       const body = {
@@ -47,10 +61,24 @@ module.exports = {
         password: hash
       };
 
-      User.create(body).then(user => resp.status(HttpStatus.CREATED).json({
-        message: "User created successfully",
-        user
-      })).catch(err => resp.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'}));
+      User.create(body)
+        .then(user => {
+          const token = jwt.sign({ user }, secret, {
+            expiresIn: 1200
+          });
+
+          resp.status(HttpStatus.CREATED).json({
+            message: "User created successfully",
+            user,
+            token
+          });
+        })
+
+        .catch(err =>
+          resp
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: "Error occured" })
+        );
     });
   }
 };
