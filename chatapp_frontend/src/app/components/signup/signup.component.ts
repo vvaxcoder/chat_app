1403a7de0279f5f10aa1c9228a365a83d7d1502a
valeriivoronkov <scorpirat@gmail.com>
2020-01-01 13:56:51 +0300

    Create service auth and add rxjs

--- chatapp_frontend/src/app/components/signup/signup.component.ts
+++ chatapp_frontend/src/app/components/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  signupUser() {
+    this.authService.signupUser('test').subscribe(data => {
+      console.log('data = ', data);
+    }, error => console.log(error));
   }
 
 }
--- chatapp_frontend/src/app/modules/auth.module.ts
+++ chatapp_frontend/src/app/modules/auth.module.ts
@@ -1,8 +1,10 @@
+import { AuthService } from './../services/auth.service';
 import { AuthTabsComponent } from './../components/auth-tabs/auth-tabs.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../components/login/login.component';
 import { SignupComponent } from '../components/signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AuthTabsComponent, LoginComponent, SignupComponent],
@@ -10,7 +12,9 @@ import { SignupComponent } from '../components/signup/signup.component';
     AuthTabsComponent, LoginComponent, SignupComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [AuthService]
 })
 export class AuthModule { }
