ec48a3848137306b8123518f9d9358421c24c56d
valeriivoronkov <scorpirat@gmail.com>
2020-01-18 21:50:40 +0300

    Generate post-form and posts components -- add post-form and posts to streams component

--- chatapp_frontend/src/app/modules/streams.module.ts
+++ chatapp_frontend/src/app/modules/streams.module.ts
@@ -4,9 +4,11 @@ import { StreamsComponent } from './../components/streams/streams.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideComponent } from '../components/side/side.component';
+import { PostFormComponent } from '../components/post-form/post-form.component';
+import { PostsComponent } from '../components/posts/posts.component';
 
 @NgModule({
-  declarations: [StreamsComponent, ToolbarComponent, SideComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent, PostFormComponent, PostsComponent],
   imports: [
     CommonModule
   ],
