1739677d311055cf74dd4f179e7e8fb271d3510d
valeriivoronkov <scorpirat@gmail.com>
2020-01-26 21:37:24 +0300

    Save post to  the DB

--- chatapp_backend/controllers/posts.js
+++ chatapp_backend/controllers/posts.js
@@ -1,6 +1,33 @@
+const Joi = require("@hapi/joi");
+const HttpStatus = require("http-status-codes");
+
+const Post = require("../models/postModel");
+
 module.exports = {
-    addPost(req, resp) {
-        console.log(req.cookies);
-        console.log(req.user);
+  addPost(req, resp) {
+    const schema = Joi.object({
+      post: Joi.string().required()
+    });
+
+    const { post } = req.body;
+
+    const { error } = schema.validate({ post });
+
+    if (error && error.details) {
+      return resp.status(HttpStatus.BAD_REQUEST).json({ msg: error.details });
     }
+
+    const body = {
+        user: req.user._id,
+        username: req.user.username,
+        post: req.user.post,
+        created: new Date()
+    };
+
+    Post.create(body).then(post => {
+        resp.status(HttpStatus.OK).json({ message: 'Post created', post });
+    }).catch(err => {
+        resp.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error occured' });
+    });
+  }
 };
