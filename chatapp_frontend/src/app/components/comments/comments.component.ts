14550c00d1d2ce13d319afc174cd04267cc463a3
valeriivoronkov <scorpirat@gmail.com>
2020-02-02 14:24:24 +0300

    Resize Post component columns; -- add AfterViewInit interface and use display=none style in it for hide -- nav-content when component is rendered

--- chatapp_frontend/src/app/components/comments/comments.component.ts
+++ chatapp_frontend/src/app/components/comments/comments.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss']
 })
-export class CommentsComponent implements OnInit {
-
+export class CommentsComponent implements OnInit, AfterViewInit {
+  
+  toolbarElement: any;
+  
   constructor() { }
-
+  
   ngOnInit() {
+    this.toolbarElement = document.querySelector('.nav-content');
   }
-
+  
+  ngAfterViewInit(): void {
+    this.toolbarElement.style.display = 'none';
+  }
 }
