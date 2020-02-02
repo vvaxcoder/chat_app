4411f6ac26d5e8fdeef66c086a006534f0662278
valeriivoronkov <scorpirat@gmail.com>
2020-02-02 13:40:04 +0300

    Allow to user add like only once

--- chatapp_backend/controllers/posts.js
+++ chatapp_backend/controllers/posts.js
@@ -67,7 +67,8 @@ module.exports = {
     const post_id = req.body._id;
 
     await Post.update(
-      { _id: post_id },
+      { _id: post_id,
+      'likes.username': { $ne: req.user.username } },
       {
         $push: {
           likes: {
